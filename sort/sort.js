be6cc96601c004687374b62c42d393f13c3dbcc5
liyang23 <liyang23@staff.weibo.com>
2016-03-09 19:18:21 +0800

    add file

---
+++ sort/sort.js
@@ -0,0 +1,23 @@
+function sort(arr, func) {
+	var ar = arr,
+		temp, result;
+
+	for (var x = 0, len = ar.length; x < len; x++) {
+		for (var y = x + 1; y < len; y++) {
+
+			result = func(ar[x], ar[y]);
+			
+			if (result > 0) {
+			    exchange(ar, x, y);
+			}
+		}
+	}
+
+	function exchange(arr, x, y) {
+		temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
+
+	return ar;
+}
