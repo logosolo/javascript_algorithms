f5834f9a104f74d6362128864634e1b0536f0aae
liyang23 <liyang23@staff.weibo.com>
2016-03-09 19:19:13 +0800

    add file

--- sort/sort.js
+++ sort/sort.js
@@ -4,9 +4,7 @@ function sort(arr, func) {
 
 	for (var x = 0, len = ar.length; x < len; x++) {
 		for (var y = x + 1; y < len; y++) {
-
-			result = func(ar[x], ar[y]);
-			
+			result = func(ar[x], ar[y]);			
 			if (result > 0) {
 			    exchange(ar, x, y);
 			}
