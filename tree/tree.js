0693d3d8bc79310540f79ff1cfe67b42db139a00
logosolo <chianwin@126.com>
2016-03-20 16:28:59 +0800

    add tree

--- tree/tree.js
+++ tree/tree.js
@@ -13,6 +13,29 @@ function BST() {
 BST.prototype = {
     inert: function(data){
         var tree = new new tree(data);         
+        
+        if(this.root == null){
+            this.root = tree;            
+        }else{
+            var current = this.root;
+            var parent;
+            while(true){
+                parent = current;
+                if(data < current.data){
+                    current = current.left;
+                    if(current == null){
+                        parent.left = tree;
+                        break;
+                    } 
+                }else{
+                    current = current.right;
+                    if(current == null){
+                        parent.right = tree;
+                        break;	
+                    }	
+                }         
+            }	
+        }
     }
 }
 
