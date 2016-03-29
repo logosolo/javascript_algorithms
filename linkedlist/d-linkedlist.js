737f3474abaa9f9395395487ac7ab727e39008cf
logosolo <chianwin@126.com>
2016-03-29 17:52:37 +0800

    improve

---
+++ linkedlist/d-linkedlist.js
@@ -0,0 +1,29 @@
+function Node(element){
+    this.element = element;
+    this.prev = null;
+    this.next = null;    
+}
+
+function DList(){
+    this.head = new Node('head');
+}
+
+DList.prototype = {
+    insert: function(newElement, item){
+        var newNode = new Node(newElement);
+        var current = this.find(item);
+        newNode.prev = current;
+        newNode.next = current.next;
+    },
+
+    remove: function(item){
+        var currNode = this.find(item);
+        if(!(currNode.next == null)){
+            currNode.prev.next = currNode.next;
+            currNode.next = null;
+            currNode.prev = null;
+        }
+    }
+
+}
+
