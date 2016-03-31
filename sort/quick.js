cb389539b8e69443a0a057e40b3ea3c7f05598cf
logosolo <chianwin@126.com>
2016-03-31 16:52:32 +0800

    add

--- sort/quick.js
+++ sort/quick.js
@@ -1,35 +1,29 @@
 function quickSort(start, end, arr) {
-
 	if (Object.prototype.toString.apply(arr) !== '[object Array]') {
 		return;
 	}
-	if (!(start < end)) {
-		return;
+	if (start < end) {
+		var q = partion(start, end, arr);
+		quickSort(start, q - 1, arr);
+		quickSort(q + 1, end, arr);
 	}
-
-	var q = partion(start, end, arr);
-	quickSort(start, q - 1, arr);
-	quickSort(q + 1, end, arr);
-
 }
 
 function partion(s, e, arr) {
 	var temp,
-		x = arr[e],
-		f = s - 1;
-
-	for (var i = s; i < e; i++) {
-		if (arr[i] <= x) {
-			f = f + 1;
+		x = arr[s],
+		f = e + 1;
+	for (var i = e; i > s; i--) {
+		if (arr[i] > x) {
+			f = f - 1;
 			temp = arr[f];
 			arr[f] = arr[i];
 			arr[i] = temp;
 		}
 	}
+	temp = arr[f - 1];
+	arr[f - 1] = arr[s];
+	arr[s] = temp;
 
-	temp = arr[f + 1];
-	arr[f + 1] = arr[e];
-	arr[e] = temp;
-
-	return f + 1;
+	return f - 1;
 }
